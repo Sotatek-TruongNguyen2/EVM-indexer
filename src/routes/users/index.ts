0bae3f376e36c3221444f945468ca97d54f20672
Arthur <truong.nguyen2@sotatek.com>
2023-08-15 19:14:33 +0700

    Remove cache

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -9,18 +9,18 @@ const USER_LEVEL_PREFIX = "USER_LEVEL";
 routes.route("/level/:address").get(async function (req, res) {
   const { address } = req.params;
 
-  const redis_client = RedisConnection.getClient();
-  const current_level_cached = await redis_client.get(
-    `${USER_LEVEL_PREFIX}_${address}`,
-  );
+  // const redis_client = RedisConnection.getClient();
+  // const current_level_cached = await redis_client.get(
+  //   `${USER_LEVEL_PREFIX}_${address}`,
+  // );
 
-  if (current_level_cached) {
-    res.status(200).send({
-      level: current_level_cached,
-      address,
-    });
-    return;
-  }
+  // if (current_level_cached) {
+  //   res.status(200).send({
+  //     level: current_level_cached,
+  //     address,
+  //   });
+  //   return;
+  // }
 
   let user = await User.findOne({
     address: {
@@ -28,20 +28,20 @@ routes.route("/level/:address").get(async function (req, res) {
     },
   });
 
-  let user_current_level = UserLevel.UNKNOWN;
+  // let user_current_level = UserLevel.UNKNOWN;
 
-  if (user) {
-    user_current_level = user.level as UserLevel;
-    // will cached the current user level in 5 minutes
-    await redis_client.setex(
-      `${USER_LEVEL_PREFIX}_${address}`,
-      300,
-      user_current_level,
-    );
-  }
+  // if (user) {
+  //   user_current_level = user.level as UserLevel;
+  //   // will cached the current user level in 5 minutes
+  //   await redis_client.setex(
+  //     `${USER_LEVEL_PREFIX}_${address}`,
+  //     300,
+  //     user_current_level,
+  //   );
+  // }
 
   res.status(200).send({
-    level: user_current_level,
+    level: user ? user.level : UserLevel.UNKNOWN,
     // branches: user ? Object.fromEntries(user.branches) : {},
     // referralBy: user?.referralBy || ethers.constants.AddressZero,
     // current_deposit: user?..current_deposit || "0",
